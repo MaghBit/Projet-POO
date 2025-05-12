744df15caa9d185542ea1a85ab0ee8c39aee9720
MaghBit <166971387+MaghBit@users.noreply.github.com>
2025-05-12 11:25:52 +1100

    Update MobHolder.py

--- MobHolder.py
+++ MobHolder.py
@@ -1,3 +1,4 @@
+#Ce fichier contient la classe MobHolder qui gère les monstres du jeu. Il permet de créer une liste de monstres possibles et de choisir un monstre aléatoire avec des statistiques différentes.
 from Enemy import *
 from random import randrange
 
@@ -6,6 +7,7 @@ class MobHolder:
     def __init__(self):
         self.possibleMob = []
 
+        #Création de la liste de monstres possibles avec leurs statistiques Ennemy(nom, vie, force)
         self.possibleMob.append(Enemy("L'Araignée", 10, 3))
         self.possibleMob.append(Enemy("Le Zombie","15",1))
         self.possibleMob.append(Enemy("La Chauve-Souris",6,5))
@@ -22,5 +24,6 @@ class MobHolder:
     def getPossibleMobNumber(self):
         return len(self.possibleMob)
     
+    #Méthode pour choisir un monstre aléatoire dans la liste de monstres possibles
     def pickRandomMob(self):
         return self.possibleMob[randrange(0,self.getPossibleMobNumber()-1)]
